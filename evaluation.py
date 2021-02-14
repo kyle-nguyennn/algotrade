97ae5c229d2dbec3fc3f9b57edd41e40a301e902
Kyle Nguyen <nghia002@outlook.com>
2021-02-14 19:15:28 +0800

    Show evaluation in report #1 - export graph to tmp dir

--- evaluation.py
+++ evaluation.py
@@ -1,15 +1,46 @@
 import pandas as pd
+import logging
+import matplotlib.pyplot as plt
+import os
+from reporting.exporter import PdfExporter
+from utils import get_logger, get_project_root
+import tempfile
+import datetime
 
-def evaluate(df: pd.DataFrame) -> bool:
+logger = get_logger(__name__, logging.INFO)
+
+def evaluate(df: pd.DataFrame, name: str, deleteTemp=False) -> bool:
     """
     Evaluate the performance of an arbitrary strategy. Columns required in df for evaluation process as below
-    :param df: assume the existence of these columns
-        value: unit value of the portfolio
-        unit_asset_ret: unit value of the asset
-    :return:
+    :param
+        df: assume the existence of these columns
+            value: unit value of the portfolio
+            unit_asset_ret: unit value of the asset
+        name: in the format stratId_assetId
+        :return:
     """
-    df[['value', 'unit_asset_ret']].plot()
+    if not deleteTemp:
+        tempDir = None
+        tempPath = os.path.join(get_project_root(), 'tmp')
+        if not os.path.exists(tempPath):
+            os.makedirs(tempPath)
+    else:
+        tempDir = tempfile.TemporaryDirectory()
+        tempPath = tempDir.name
+    tempPath = os.path.join(tempPath, name + '_' + datetime.datetime.now().strftime("%Y-%m-%dH%H:%M:%S"))
+    fig = plt.figure(figsize=(15, 10))
+    s1 = fig.add_subplot(2, 1, 1)
+    s1.plot(df['value'], label='Portfolio unit value')
+    s1.plot(df['unit_asset_ret'], label='Asset unit value')
+    s1.legend()
+    s1.set_title("Return")
+    s2 = fig.add_subplot(2, 1, 2)
     alpha = df['value'] - df['unit_asset_ret']
-    alpha.plot()
+    s2.plot(alpha, label='Excess return compared to asset', color='r')
+    s2.legend()
+    s2.set_title("Alpha")
+    fig.savefig(tempPath, format='png')
+    logger.info(f"Graph for {name} generated at {tempPath}.")
+    if tempDir: tempDir.cleanup()
     ### TODO: define the criteria for a pass
     return True
--- strategies/ThreeHeadStrategy.py
+++ strategies/ThreeHeadStrategy.py
@@ -4,19 +4,20 @@ from indicators.indicators_impl import MovingWindow
 from strategies.strategy_interface import Strategy
 import pandas as pd
 import numpy as np
+import typing
 
 class ThreeHeadStrategyImpl(Strategy):
     ma_type: str # type of moving average, in list ['SMA', 'EMA']
-    short_allowed: bool # whether short selling is allowed, default is False
     cur_window_size: int # shortest window size to calculate moving average
     ceil_window_size: int # middle-sized window size to calculate moving average
     floor_window_size: int # longest window size to calculate moving average
+    short_allowed: bool # whether short selling is allowed, default is False
 
-    def __init__(self, **kwargs):
+    def __init__(self, id:str, **kwargs):
         """
         :param kwargs: including the following attributes
         """
-        super().__init__(**kwargs)
+        super().__init__(id, **kwargs)
         assert(self.cur_window_size < self.ceil_window_size < self.floor_window_size)
         self.indicators = [
             MovingWindow(['Adj Close'], ['cur'], window_size=self.cur_window_size, aggregate='mean'),
@@ -28,26 +29,28 @@ class ThreeHeadStrategyImpl(Strategy):
         data = IndicatorRunner().run(data, self.indicators)
         return data
 
-    def run(self, data: pd.DataFrame) -> pd.DataFrame:
+    def run(self, data: typing.Mapping[str, pd.DataFrame]) -> pd.DataFrame:
         """
         :param data:
         :return: a series of buy/sell signal
         """
         ### this step should be implemented in Indicator class
-        df = data
+        assetId = list(data.keys())[0]
+        df = data[assetId]
+        df = df.dropna()
         ### enrich data required for strategy implementation
         df = self.__calculateIndicators(df)
         ### actual strategy ###
-        p = df[['cur', 'ceil', 'floor']].tail(11).plot()
         df['position'] = np.where((df['cur'] > df['ceil']) & (df['ceil'] > df['floor']), 1, 0)
         ### evaluation parameters
         df['asset_ret'] = df['Adj Close'].pct_change()
         df['return'] = df['asset_ret'] * df['position']
-        df['unit_asset_ret'] = df['Adj Close'] / df['Adj Close'][0]
+        df['unit_asset_ret'] = (1 + df['asset_ret']).cumprod()
         df['value'] = (1 + df['return']).cumprod()
         ### evaluate
-        if not evaluate(df):
-            print(f"Strategy {self.__name__} is not suitable for this asset.")
+        evalName = f"{self.id}_{assetId}"
+        if not evaluate(df, evalName):
+            print(f"Strategy {self.__name__} is not suitable for this asset [{assetId}].")
             return None
         ### generate buy/sell signal if pass evaluation phase
         """
